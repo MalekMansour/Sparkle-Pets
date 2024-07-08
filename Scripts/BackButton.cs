da2c8b54e7acd2392fdd3f9038d33c28630a02e5
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2024-07-09 00:23:01 +0100

    Update BackButton.cs

--- Scripts/BackButton.cs
+++ Scripts/BackButton.cs
@@ -1,1 +1,27 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackButton : MonoBehaviour
+{
+    public GameObject pauseMenu;    // Reference to the pause menu panel
+    public GameObject settingsMenu; // Reference to the settings menu panel
+    public Button backButton;       // Reference to the back button
+
+    void Start()
+    {
+        if (backButton != null)
+        {
+            backButton.onClick.AddListener(BackToPauseMenu);
+        }
+    }
+
+    void BackToPauseMenu()
+    {
+        if (pauseMenu != null && settingsMenu != null)
+        {
+            settingsMenu.SetActive(false); // Hide the settings menu
+            pauseMenu.SetActive(true);     // Show the pause menu
+        }
+    }
+}
 
