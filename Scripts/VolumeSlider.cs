8edabafe8ac8c766937a7adc881bd138e33b688f
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2024-07-26 02:15:01 +0100

    Update VolumeSlider.cs

--- Scripts/VolumeSlider.cs
+++ Scripts/VolumeSlider.cs
@@ -3,15 +3,15 @@ using UnityEngine.UI;
 
 public class VolumeSlider : MonoBehaviour
 {
-    public AudioSource musicSource; // Reference to the AudioSource component playing the music
-    public Slider volumeSlider;     // Reference to the UI slider
+    public AudioSource musicSource; 
+    public Slider volumeSlider;     
 
     void Start()
     {
         if (volumeSlider != null)
         {
             volumeSlider.onValueChanged.AddListener(AdjustVolume);
-            volumeSlider.value = musicSource.volume; // Initialize slider value to the current volume
+            volumeSlider.value = musicSource.volume; 
         }
     }
 
@@ -23,4 +23,3 @@ public class VolumeSlider : MonoBehaviour
         }
     }
 }
-
