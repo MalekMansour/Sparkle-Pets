7bb5381101739713764a1e727255c5269970dade
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2024-07-27 02:37:29 +0100

    Update VolumeSlider.cs

--- Scripts/VolumeSlider.cs
+++ Scripts/VolumeSlider.cs
@@ -1,17 +1,23 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class VolumeSlider : MonoBehaviour
 {
-    public AudioSource musicSource; 
-    public Slider volumeSlider;     
+    public AudioSource musicSource;     
+    public Slider volumeSlider;         
+    public TextMeshProUGUI volumeText;  
 
     void Start()
     {
         if (volumeSlider != null)
         {
+            volumeSlider.minValue = 0;
+            volumeSlider.maxValue = 1;
+            volumeSlider.wholeNumbers = false;
+            volumeSlider.value = 1f; 
             volumeSlider.onValueChanged.AddListener(AdjustVolume);
-            volumeSlider.value = musicSource.volume; 
+            UpdateVolumeText(volumeSlider.value); 
         }
     }
 
@@ -20,6 +26,16 @@ public class VolumeSlider : MonoBehaviour
         if (musicSource != null)
         {
             musicSource.volume = volume;
+            UpdateVolumeText(volume); 
+        }
+    }
+
+    void UpdateVolumeText(float volume)
+    {
+        if (volumeText != null)
+        {
+            int volumePercentage = Mathf.RoundToInt(volume * 100);
+            volumeText.text = volumePercentage + "%";
         }
     }
 }
